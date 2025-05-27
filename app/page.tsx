f7175b5380ad1ba6a1adeee93c282951780ac58f
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2025-05-28 00:15:23 +0200

    feat. add popular companion

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,46 @@
 import React from 'react'
-import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 const Page = () => {
-  return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to AI Voice</h1>
-      <Button>Let's get started</Button>
-    </div>
-  )
+	return (
+		<main>
+			<h1>Popular Companions</h1>
+
+			<section className="home-section">
+				<CompanionCard
+					id="123"
+					name="Neura the Brainy Explorer"
+					topic="Neural Network of the Brain"
+					subject="Science"
+					duration={45}
+					color="#ffda6e"
+				/>
+				<CompanionCard
+					id="456"
+					name="Countsy the Number Wizard"
+					topic="Derivatives & Integrals"
+					subject="Maths"
+					duration={45}
+					color="#e5d0ff"
+				/>
+				<CompanionCard
+					id="789"
+					name="Verba the Vocabulary Builder"
+					topic="English Literature"
+					subject="Language"
+					duration={30}
+					color="#bde7ff"
+				/>
+			</section>
+
+			<section className="home-section">
+				<CompanionsList />
+				<CTA />
+			</section>
+		</main>
+	)
 }
 
 export default Page
